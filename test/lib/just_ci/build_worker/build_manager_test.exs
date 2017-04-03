2691006bb71af12d10c51778e7d439de2bd24ed9
Sam Jeston <sam.jeston@gmail.com>
2017-04-04 08:55:14 +1000

    Travis rework

--- test/lib/just_ci/build_worker/build_manager_test.exs
+++ test/lib/just_ci/build_worker/build_manager_test.exs
@@ -1,11 +1,13 @@
 defmodule JustCi.BuildManagerTest do
   use ExUnit.Case
 
-  test "stores values by key" do
-    {:ok, bucket} = JustCi.BuildManager.start_link
-    assert JustCi.BuildManager.get(bucket, "milk") == nil
+  test "stores new job in the agent" do
+    job = %{job_id: 1}
 
-    JustCi.BuildManager.put(bucket, "milk", 3)
-    assert JustCi.BuildManager.get(bucket, "milk") == 3
+    {:ok, bucket} = JustCi.BuildManager.start_link
+    assert JustCi.BuildManager.get_jobs == []
+
+    JustCi.BuildManager.push(job)
+    assert JustCi.BuildManager.get_jobs == [job]
   end
 end
