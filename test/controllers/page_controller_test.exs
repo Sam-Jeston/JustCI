dd44a6da95e6c669bcacb104af615893b17428be
Sam Jeston <sam.jeston@gmail.com>
2017-04-10 08:51:44 +1000

    Correct PageController test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule JustCi.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Your CI"
   end
 end
