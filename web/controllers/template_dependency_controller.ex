55f56297ee66cfa8f01b266796ecb7aa4f2d5943
Sam Jeston <sam.jeston@gmail.com>
2017-05-11 08:56:49 +1000

    View logic for dependency selection. WIP Form

--- web/controllers/template_dependency_controller.ex
+++ web/controllers/template_dependency_controller.ex
@@ -7,11 +7,35 @@ defmodule JustCi.TemplateDependencyController do
 
   def view(conn, %{"id" => id}) do
     template = Template |> Repo.get(id) |> Repo.preload(:dependencies)
-    IO.puts "~~~ Shit Our dependency is loading!!!"
-    IO.inspect template
-    render(conn, "view.html", template: template)
+    dependencies = Dependency |> Repo.all
+    transformed_dependencies = transform_dependencies(template.dependencies, dependencies)
+
+    IO.inspect transformed_dependencies
+    render(conn, "view.html", template: template, dependencies: transformed_dependencies)
   end
 
   def link(conn, _params) do
+  end
+
+  def transform_dependencies(template_dependencies, all_dependencies) do
+    selected_dependencies = Enum.map(template_dependencies, &(%{
+      dependency_id: &1.id,
+      selected: true,
+      command: &1.command
+    }))
+
+    selected_dependency_ids = Enum.map(selected_dependencies, &(&1[:dependency_id]))
+    dependency_ids = Enum.map(all_dependencies, &(&1.id))
+    unselected_dependency_ids = dependency_ids -- selected_dependency_ids
+
+    dep_unselected = fn dep -> Enum.member?(unselected_dependency_ids, dep.id) end
+    unselected_dependencies = Enum.filter(all_dependencies, dep_unselected)
+    unselected_dependencies_transform = Enum.map(unselected_dependencies, &(%{
+      dependency_id: &1.id,
+      selected: false,
+      command: &1.command
+    }))
+
+    selected_dependencies ++ unselected_dependencies_transform
   end
 end
